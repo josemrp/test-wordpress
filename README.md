e7f82a38bc8eec39b2fb985284e2b9c35d8e5b4d
josemrp <josemanuelromerop@gmail.com>
2018-06-16 17:47:49 -0400

    Child theme

---
+++ README.md
@@ -0,0 +1,5 @@
+## Themes
+
+https://developer.wordpress.org/themes/
+
+* [Child Themes](https://developer.wordpress.org/themes/advanced-topics/child-themes/)
---
+++ wp-content/themes/twentyfifteen-child/functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This is load after parent functions are loaded
+ */
+
+ //Get the parent's style
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+ 
+}
+?>
